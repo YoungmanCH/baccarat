d02149ea93ab6e53a1313d3cb21c33425ddb7078
YoungmanCH <tsubasa22business@gmail.com>
2023-02-05 00:20:29 +0900

    Save baccarat.py third It's not perfect.

--- baccarat.py
+++ baccarat.py
@@ -79,7 +79,8 @@ def game():
   Club8 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
   Club = [Club1, Club2, Club3, Club4, Club5, Club6, Club7, Club8]
   cards = [Diamond, Spade, Heart, Club]
-  player_first_result = []
+  player_result = []
+  player_count_result = []
   c1 = 0
 
   while c1 != 2:
@@ -88,16 +89,17 @@ def game():
     n3 = r.randint(0, 3)
 
     try:
-      player_first_result.append(cards[n3][n2][n1])
+      player_result.append(cards[n3][n2][n1])
       del cards[n3][n2][n1]
       c1 += 1
     except IndexError as e2:
       continue
 
   print("Player's card")
-  print(player_first_result[0], player_first_result[1])
-
-  banker_first_result = []
+  print(player_result[0], player_result[1])
+  player_count_result = player_result
+  banker_result = []
+  banker_count_result = []
   c2 = 0
 
   while c2 != 2:
@@ -106,33 +108,34 @@ def game():
     n3 = r.randint(0, 3)
 
     try:
-      banker_first_result.append(cards[n3][n2][n1])
+      banker_result.append(cards[n3][n2][n1])
       del cards[n3][n2][n1]
       c2 += 1
     except IndexError as e3:
       continue
 
   print("Banker's card")
-  print(banker_first_result[0], banker_first_result[1])
+  print(banker_result[0], banker_result[1])
+  banker_count_result = banker_result
   first_judge = None
-  player_first_sum = None
-  banker_first_sum = None
+  player_sum = None
+  banker_sum = None
 
-  if player_first_result[0] == "J" or player_first_result[0] == "Q" or player_first_result[0] == "K":
-    player_first_result[0] = "10"
-  if player_first_result[1] == "J" or player_first_result[1] == "Q" or player_first_result[1] == "K":
-    player_first_result[1] = "10"
-  player_first_sum = int(player_first_result[0])+int(player_first_result[1])
+  if player_result[0] == "J" or player_result[0] == "Q" or player_result[0] == "K":
+    player_result[0] = "10"
+  if player_result[1] == "J" or player_result[1] == "Q" or player_result[1] == "K":
+    player_result[1] = "10"
+  player_first_sum = int(player_result[0])+int(player_result[1])
   if player_first_sum >= 10:
     player_first_sum -= 10
   if player_first_sum == 8 or player_first_sum == 9:
     first_judge = "Natural"
     
-  if banker_first_result[0] == "J" or banker_first_result[0] == "Q" or banker_first_result[0] == "K":
-    banker_first_result[0] = "10"
-  if banker_first_result[1] == "J" or banker_first_result[1] == "Q" or banker_first_result[1] == "K":
-    banker_first_result[1] = "10"
-  banker_first_sum = int(banker_first_result[0])+int(banker_first_result[1])
+  if banker_result[0] == "J" or banker_result[0] == "Q" or banker_result[0] == "K":
+    banker_result[0] = "10"
+  if banker_result[1] == "J" or banker_result[1] == "Q" or banker_result[1] == "K":
+    banker_result[1] = "10"
+  banker_first_sum = int(banker_result[0])+int(banker_result[1])
   if banker_first_sum >= 10:
     banker_first_sum -= 10
   if banker_first_sum == 8 or banker_first_sum == 9:
@@ -141,12 +144,45 @@ def game():
   if first_judge == "Natural":
     if player_first_sum > banker_first_sum:
       print("Player is winner!")
-    elif player_first_sum == banker_first_result:
+    elif player_first_sum == banker_first_sum:
       print("Tie")
     else:
       print("Banker is winner!")
   else:
     print("Next cards!")
+    c3 = 0
+
+    while c3 !=1:
+      n1 = r.randint(0, 12)
+      n2 = r.randint(0, 7)
+      n3 = r.randint(0, 3)
+      try:
+        player_result.append(cards[n3][n2][n1])
+        player_count_result.append(player_result[2])
+        del cards[n3][n2][n1]
+        c3 += 1
+      except IndexError as e3:
+        continue
+
+    print("Player's card")
+    print(player_count_result[0], player_count_result[1], player_count_result[2])
+    c4 = 0
+
+    while c4 !=1:
+      n1 = r.randint(0, 12)
+      n2 = r.randint(0, 7)
+      n3 = r.randint(0, 3)
+      try:
+        banker_result.append(cards[n3][n2][n1])
+        banker_count_result.append(banker_result[2])
+        del cards[n3][n2][n1]
+        c4 += 1
+      except IndexError as e4:
+        continue
+
+    print("Banker's card")
+    print(banker_count_result[0], banker_count_result[1], banker_count_result[2])
+    
 
 # preparation()
 game()
